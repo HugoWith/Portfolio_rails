f84a4bc1bd4f17f288879feb056228bbe91b6f45
Hugo Valla <Hugo_valla@hotmail.fr>
2020-01-09 16:49:11 +0100

    contact form WIP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,4 @@
 class PagesController < ApplicationController
   def home
-  
   end
-
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'home/index'
+  post 'home/index'
   root to: 'pages#home'
+  resources :pages, only: [:home, :new, :create]
 end
