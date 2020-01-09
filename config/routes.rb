e1b20663aa8aa0f5eb152b4b7dd8caf790754f17
Hugo Valla <Hugo_valla@hotmail.fr>
2020-01-09 17:55:23 +0100

    env

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/home'
   get 'home/index'
-  post 'home/index'
   root to: 'pages#home'
-  resources :pages, only: [:home, :new, :create]
+  resources :home, only: [:home, :new, :create]
 end
